e3a4e4c292598ec4b02e03b246a5d4823045344c
SteveVolbrecht <svolbrecht@yahoo.com>
2019-04-25 12:04:09 -0500

    learning basic passing and using of data with event binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
@@ -17,6 +17,7 @@ import { WarningAlertComponent } from './warning-alert/warning-alert.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
